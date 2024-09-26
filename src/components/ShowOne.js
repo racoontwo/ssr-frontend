b440d3eccb20d45a7ce33b8e6fa71e1a835b1e91
racoon2 <oliver.roosvall@gmail.com>
2024-09-26 11:09:30 +0200

    cleaned up according to lint.

--- src/components/ShowOne.js
+++ src/components/ShowOne.js
@@ -1,34 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 export default function ShowOne({ item }) {
     const [formData, setFormData] = useState({
         _id: item._id,
         title: item.title,
-        content: item.content
+        content: item.content,
     });
 
     const handleChange = (e) => {
         setFormData({
             ...formData,
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value,
         });
     };
 
     const handleSubmit = async (e) => {
-        e.preventDefault(); 
+        e.preventDefault();
         try {
             // const response = await fetch("http://localhost:3001/posts/update_docs", {
-            const response = await fetch("https://jsramverk-editor-olrs23-g3bthketdnh3bag4.northeurope-01.azurewebsites.net/posts/update_docs", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
+            const response = await fetch(
+                'https://jsramverk-editor-olrs23-g3bthketdnh3bag4.northeurope-01.'
+                +'azurewebsites.net/posts/update_docs',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(formData),
                 },
-                body: JSON.stringify(formData)
-            });
+            );
             const data = await response.json();
-            console.log("Response from backend:", data);
+
+            console.log('Response from backend:', data);
         } catch (error) {
-            console.error("Error submitting form:", error);
+            console.error('Error submitting form:', error);
         }
     };
 
@@ -38,17 +43,27 @@ export default function ShowOne({ item }) {
 
     return (
         <div>
-        <form onSubmit={handleSubmit}>
-            <label>
-                Title:
-                <input type="text" name="title" value={formData.title} onChange={handleChange} />
-            </label>
-            <label>
-                Content:
-                <input type="text" name="content" value={formData.content} onChange={handleChange} />
-            </label>
-            <button type="submit">Update</button>
-        </form>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Title:
+                    <input
+                        type="text"
+                        name="title"
+                        value={formData.title}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Content:
+                    <input
+                        type="text"
+                        name="content"
+                        value={formData.content}
+                        onChange={handleChange}
+                    />
+                </label>
+                <button type="submit">Update</button>
+            </form>
         </div>
     );
 }
