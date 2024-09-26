7a99e2b9da580677dae124224d6da454d36d9873
racoon2 <oliver.roosvall@gmail.com>
2024-09-26 10:43:42 +0200

    testing to connect frontend to deployed backend.

--- src/components/ShowDocs.js
+++ src/components/ShowDocs.js
@@ -13,7 +13,9 @@ export default function ShowDocs({ setPage, setSelectedItem }) {
     useEffect(() => {
         async function fetchData() {
         try {
-            const response = await fetch('http://localhost:3001/posts/json');
+            // const response = await fetch('http://localhost:1337/posts/json');
+            // const response = await fetch('https://jsramverk-editor-olrs23-g3bthketdnh3bag4.northeurope-01.azurewebsites.net/posts/json');
+            const response = await fetch('https://jsramverk-editor-olrs23-g3bthketdnh3bag4.northeurope-01.azurewebsites.net/posts/json');
             // const response = await fetch('https://trafik.emilfolino.se/stations');
             if (!response.ok) {
             throw new Error('Network response was not ok');
--- src/components/ShowOne.js
+++ src/components/ShowOne.js
@@ -17,7 +17,8 @@ export default function ShowOne({ item }) {
     const handleSubmit = async (e) => {
         e.preventDefault(); 
         try {
-            const response = await fetch("http://localhost:3001/posts/update_docs", {
+            // const response = await fetch("http://localhost:1337/posts/update_docs", {
+            const response = await fetch("https://jsramverk-editor-olrs23-g3bthketdnh3bag4.northeurope-01.azurewebsites.net/posts/update_docs", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
